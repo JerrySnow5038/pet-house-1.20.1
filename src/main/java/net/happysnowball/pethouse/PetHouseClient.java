b53f0b4a3cc1d24b90c657fa8d5106873e987203
JerrySnow5038 <1261988879@qq.com>
2025-08-25 00:35:02 +0800

    The first submitting. The place where the dream was born.

---
+++ src/main/java/net/happysnowball/pethouse/PetHouseClient.java
@@ -0,0 +1,10 @@
+package net.happysnowball.pethouse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PetHouseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
